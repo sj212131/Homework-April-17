8614864b95d9bcb240881a7584f29040bd37850c
shangxian Piao <sj212131@hotmail.com>
2021-04-11 17:21:11 -0400

    updated server.js

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,5 +1,6 @@
 var $noteTitle = $(".note-title");
 var $noteText = $(".note-textarea");
+var $backBtn = $(".backBtn");
 var $saveNoteBtn = $(".save-note");
 var $newNoteBtn = $(".new-note");
 var $noteList = $(".list-container .list-group");
